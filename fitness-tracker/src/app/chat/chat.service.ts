d7976fc417ffbddfb9a17bba5eb50bbd8bf471f1
DESKTOP-EI787M1\Ramm <bestcoder0@yahoo.com>
2018-05-30 20:46:21 -0400

    chat delta

--- fitness-tracker/src/app/chat/chat.service.ts
+++ fitness-tracker/src/app/chat/chat.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
 import {AngularFirestore} from 'angularfire2/firestore';
+import { Subscription, Subject } from 'rxjs';
+import { IM } from './IM.model';
+import { UIService } from '../shared/ui.service';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class ChatService {
 
-  constructor() { }
+  private imSubs : Subscription[] = [];
+  imsChanged = new Subject<IM[]>();
+  private prevIms : IM[] = []; 
+
+  constructor(private db: AngularFirestore, private uiService : UIService, 
+      private authService: AuthService) { }
+
+      fetchExercises() {
+        this.uiService.loadingStateChanged.next(true);
+        this.db.collection("messages")
+        .ref
+        .orderBy("sentTS","desc")
+        .limit(500);
+
+        this.imSubs.push(this.db.collection('messages', ref => 
+        ref.orderBy("sentTS","desc")
+        .limit(500))
+        .valueChanges()
+        .subscribe((messages: IM[]) =>{
+            this.prevIms = messages;
+            this.imsChanged.next(messages);
+        }));
+      
+    }
+
+
+    sendIM(message : IM) {
+      message.userid =  this.authService.getUser().userId;
+      this.db.collection('messages').add(message);
+    }
+
 
 }
