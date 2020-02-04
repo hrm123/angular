bb773a2ce3a6065e42a23207fcbec299dd6d5963
DESKTOP-EI787M1\Ramm <bestcoder0@yahoo.com>
2020-02-04 16:23:24 -0500

    ui files added

--- teamchat/src/app/app-routing.module.ts
+++ teamchat/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { SignonFormComponent } from './signon-form/signon-form.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'signup', component: SignupFormComponent},
+  {path:'signon', component: SignonFormComponent},
+  {path:'chat', component: ChatRoomComponent},
+  {path:'', redirectTo: 'signon', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- teamchat/src/app/app.module.ts
+++ teamchat/src/app/app.module.ts
@@ -3,16 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthService } from './services/auth.service';
+import { ChatService } from './services/chat.service';
+import { ChatFormComponent } from './chat-form/chat-form.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
+import { FeedComponent } from './feed/feed.component';
+import { MessageComponent } from './message/message.component';
+import { SignonFormComponent } from './signon-form/signon-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatFormComponent,
+    ChatRoomComponent,
+    FeedComponent,
+    MessageComponent,
+    SignonFormComponent,
+    SignupFormComponent,
+    NavbarComponent,
+    UserListComponent,
+    UserItemComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [AuthService, ChatService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
