78279fc7fe2014f2b7f016700f9a7796772e2784
DESKTOP-EI787M1\Ramm <bestcoder0@yahoo.com>
2020-02-04 15:37:16 -0500

    scaffolded code ng8

--- teamchat/src/app/app.module.ts
+++ teamchat/src/app/app.module.ts
@@ -1,34 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChatFormComponent } from './chat-form/chat-form.component';
-import { ChatRoomComponent } from './chat-room/chat-room.component';
-import { FeedComponent } from './feed/feed.component';
-import { MessageComponent } from './message/message.component';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { SignonFormComponent } from './signon-form/signon-form.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { UserItemComponent } from './user-item/user-item.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ChatFormComponent,
-    ChatRoomComponent,
-    FeedComponent,
-    MessageComponent,
-    LoginFormComponent,
-    SignonFormComponent,
-    NavBarComponent,
-    UserListComponent,
-    UserItemComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
