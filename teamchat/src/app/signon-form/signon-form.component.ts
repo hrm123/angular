8ef7e620b9ce7c3a0058c6bca3eea0e2a4535cd8
DESKTOP-EI787M1\Ramm <bestcoder0@yahoo.com>
2020-02-04 13:56:05 -0500

    scaffolded code

---
+++ teamchat/src/app/signon-form/signon-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signon-form',
+  templateUrl: './signon-form.component.html',
+  styleUrls: ['./signon-form.component.css']
+})
+export class SignonFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
