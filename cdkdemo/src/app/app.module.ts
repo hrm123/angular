3a4f5024890a0769d25429d11505148f83333574
h_r_m <pitechworks@gmail.com>
2023-05-19 10:02:28 -0700

    side nav

--- cdkdemo/src/app/app.module.ts
+++ cdkdemo/src/app/app.module.ts
@@ -6,16 +6,30 @@ import { AppComponent } from './app.component';
 import { SbModule } from './sb/sb.module';
 import { SbComponent } from './sb/sb.component';
 import { LandingComponent } from './landing/landing.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
+    MainNavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SbModule.forRoot()
+    SbModule.forRoot(),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
